10a10d8b992ed64eeb816da8116a73f6d798a8e0
Kamil Bednarski <kamilarturbednarski@gmail.com>
2020-06-01 17:45:08 +0200

    Initial commit

---
+++ src/main/java/io/github/mat3e/todo/TodoServlet.java
@@ -0,0 +1,50 @@
+package io.github.mat3e.todo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Todo", urlPatterns = {"/api/todos/*"})
+public class TodoServlet extends HttpServlet {
+    private final Logger logger = LoggerFactory.getLogger(TodoServlet.class);
+
+    private TodoRepository repository;
+    private ObjectMapper mapper;
+
+    /**
+     * Servlet container needs it.
+     */
+    @SuppressWarnings("unused")
+    public TodoServlet() {
+        this(new TodoRepository(), new ObjectMapper());
+    }
+
+    TodoServlet(TodoRepository repository, ObjectMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Got request with parameters: " + req.getParameterMap());
+        resp.setContentType("application/json;charset=UTF-8");
+        mapper.writeValue(resp.getOutputStream(), repository.findAll());
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var pathInfo = req.getPathInfo();
+        try {
+            var todoId = Integer.valueOf(pathInfo.substring(1));
+        } catch (NumberFormatException e) {
+            logger.warn("Wrong path: " + pathInfo);
+        }
+    }
+}
